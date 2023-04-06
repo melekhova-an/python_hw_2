9021287cbc1f8bef81b101504b3be987f3e4965d
Anastasiya Melekhova <amelekhova-aan@yandex.ru>
2023-04-06 11:33:44 +0300

    did the second homework with pytest and selene

--- test_homework_2.py
+++ test_homework_2.py
@@ -9,7 +9,13 @@ def open_browser():
     browser.open('https://google.com')
     pass
 
-def test_nothing_found(open_browser):
+def test_first_test(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    return 'Selene - User-oriented Web UI browser tests in Python'
+
+def test_nothing_found():
+    browser.element('[aria-label="Очистить"]').click()
     browser.element('[name="q"]').should(be.blank).type('qwertyuiopasdfgghhjklzxc').press_enter()
     browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
     return "Ничего не нашлось"
